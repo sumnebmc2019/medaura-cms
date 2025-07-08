62f4a90cdb5d9dea66186f85b33b53b254e6e9ce
sumnebmc2019 <sumnebmc2019@gmail.com>
2025-07-08 20:17:01 +0530

    Enable Cloudinary previews via CSP

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,18 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
